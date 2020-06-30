0501a8a67931d31394fea6f50ced5ce99371c575
leaf <1769120379@qq.com>
2020-06-30 17:01:06 +0800

    注册完成

--- web/client/js/register.js
+++ web/client/js/register.js
@@ -1,3 +1,84 @@
 $(() => {
-    console.log(aa)
+    $("#userID").blur(function () {
+        if ($("#userID").val() == "") {
+            alert("用户名不能为空")
+            return;
+        }
+        let reg = /^[a-zA-Z]\d{5,8}$/;
+        let val = $.trim($(this).val());
+        if (reg.test(val)) {
+            $(this).next().text("");
+        } else {
+            $(this).next().text("格式不正确");
+            $(this).next().css("color", "red");
+        }
+    });
+
+    $("#passID").blur(function () {
+        if ($("#passID").val() == "") {
+            alert("密码不能为空")
+            return;
+        }
+        let reg = /^[a-z0-9]{6,9}$/;
+        let val = $.trim($(this).val());
+        if (reg.test(val)) {
+            $(this).next().text("");
+        } else {
+            $(this).next().text("格式不正确");
+            $(this).next().css("color", "red");
+        }
+    })
+
+    $("#emailID").blur(function () {
+        if ($("#emailID").val() == "") {
+            alert("email不能为空")
+            return;
+        }
+        let reg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+        let val = $.trim($(this).val());
+        if (reg.test(val)) {
+            $(this).next().text("");
+        } else {
+            $(this).next().text("格式不正确");
+            $(this).next().css("color", "red");
+        }
+    })
+
+    $("#zcbtn").click(function () {
+        if ($("#userID,#passID,#emailID").val() == "") {
+            alert("输入信息")
+            return;
+        }
+        if ($("#userID,#passID,#emailID").next().text() != "") {
+            alert("验证失败")
+            return;
+        } else {
+            let data = {
+                username: $.trim($("#userID").val()),
+                password: $.trim($("#passID").val()),
+                email: $.trim($("#emailID").val())
+            }
+
+            $.ajax({
+                type: "post",
+                dataType: "json",
+                url: "../server/register.php",
+                data,
+                success(response) {
+                    JSON.stringify(response);
+                    if (response.status == "success") {
+                        alert(response.msg);
+                    } else {
+                        alert(response.msg);
+                    }
+                }
+
+
+            });
+
+
+        }
+
+    });
+
 })
--- web/server/register.php
+++ web/server/register.php
@@ -1,29 +1,29 @@
 <?php
-include_once "./commonDB.php";
+	
+$db = mysqli_connect("localhost", "root", "root", "leaf");
 
 
-$username=$_REQUEST["username"];
-$password=$_REQUEST["password"];
-$email=$_REQUEST["email"];
+$username = $_REQUEST["username"];
+$password = $_REQUEST["password"];
+$email    = $_REQUEST["email"];
 
-$sql = "SELECT * FROM `user` WHERE username = '$username'";
-$r = mysqli_query($db, $sql);
-$num= mysqli_num_rows($r);
-if($num == 1){
-    echo '{"status":"error","msg":"该用户已经存在，请重新填写注册的昵称!!"}';
-    
-  }else{
-    $sql = "INSERT INTO user " .
-      "(id,username,password,user_date,phone)" .
-      "VALUES " .
-      "(NULL,'$username','$password','2020-06-28',$phone)";
+$sql = "SELECT * FROM user WHERE username='$username'";
+// print_r($sql);
+# 执行SQL语句
+$result = mysqli_query($db,$sql);
+
+
+if(mysqli_num_rows($result) == 0)
+{
+
+$sql = "INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES (NULL, '$username', '$password', '$email')";
+$result = mysqli_query($db, $sql);
   
-    $retval = mysqli_query($db, $sql);
-  
-    if (!$retval) {
-      die('无法插入数据: ' . mysqli_error($conn));
-    }
-  
-    echo '{"status":"success"}';
-  }
+// $arr = array("status"=>"success","msg"=> "恭喜你，注册成功！");
+// echo json_encode($arr);
+  echo '{"status":"success","msg":"恭喜你，注册成功！"}';
+}else{
+  # 该用户已经存在，不能再注册
+  echo '{"status":"error","msg":"抱歉，该用户名已经被注册，请重新选择一个更优秀的名字！！"}';
+}
 ?>
