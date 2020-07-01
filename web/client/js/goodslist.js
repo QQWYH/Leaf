793d539fcf25beb9ae4e2c0f3c745f0e3892e786
leaf <1769120379@qq.com>
2020-07-01 10:20:44 +0800

    商品列表

--- web/client/js/goodslist.js
+++ web/client/js/goodslist.js
@@ -5,19 +5,20 @@ $(() => {
         dataType: "json",
         success: function (data) {
             console.log(data)
+
             let html = data.map(item => {
                 return `
                 <div class="list-show">
-                <div class="img">${data.img}</div>
+                <div class="img"><img src="${item.img}"></div>
                 <div class="goods-info">
-                    <span>${data.title}</span>
+                    <span>${item.title}</span>
                 </div>
                 <div class="goods-box">
                     <div class="goods-title">
-                        <span>${data.titleA}</span>
+                        <span>${item.titleA}</span>
                     </div>
                     <div class="goods-bottom">
-                        <span>${data.prive}</span>
+                        <span>${item.prive}</span>
                     </div>
                 </div>
             </div>               
