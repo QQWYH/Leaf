c3abafea459413b63654891a860359e7eb722c62
leaf <1769120379@qq.com>
2020-07-03 17:32:40 +0800

    修改

--- web/client/js/tony.js
+++ web/client/js/tony.js
@@ -2,7 +2,7 @@ $(() => {
     let name = window.localStorage.getItem("username") || [];
     if (name != "") {
         $(".name>a").css("display", "none");
-        $(".name>span").text(name, '<a href = ""> 注销</a>').css("color", "#fff");
+        $(".name>span").text(name, +'<a href="注销"></a>').css("color", "#fff");
     } else {
         alert("请登录");
     }
