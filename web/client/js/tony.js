cb0ebc335e38ac054ba50ab50cab45524e595702
leaf <1769120379@qq.com>
2020-07-03 12:00:02 +0800

    登录完成

--- web/client/js/tony.js
+++ web/client/js/tony.js
@@ -2,7 +2,7 @@ $(() => {
     let name = window.localStorage.getItem("username") || [];
     if (name != "") {
         $(".name>a").css("display", "none");
-        $(".name>span").text(name, +"欢迎您")
+        $(".name>span").text(name, +"欢迎您" + "<a href=''>注销</a>")
     } else {
         alert("请登录");
     }
