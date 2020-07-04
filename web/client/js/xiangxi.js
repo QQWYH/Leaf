2db97a19a63bfd01f2bd4774fa041a572db152cd
leaf <1769120379@qq.com>
2020-07-04 09:02:14 +0800

    修改

--- web/client/js/xiangxi.js
+++ web/client/js/xiangxi.js
@@ -40,45 +40,4 @@ $(() => {
 
 
 
-    $.ajax({
-        type: "get",
-        data: { url },
-        url: "./../server/nvgoodslist.php",
-        dataType: "json",
-        success: function (data) {
-            console.log(data)
-            let html = data.map(item => {
-                return `
-                <div class="p-con">
-                <div class="p-left"><img src="${item.img}"></div>
-                <div class="p-right">
-                    <div class="title">${item.title}</div>
-                    <div class="titleA">${item.titleA}</div>
-                    <div class="prive">${item.prive}</div>
-                    <div class="cout">
-                        <div class="yds">
-                            <select name="num" id="select">
-                                <option>42</option>
-                                <option>43</option>
-                                <option>44</option>
-                                <option>45</option>
-                            </select>
-                        </div>
-                        <div class="count">
-                            <input type="text" id="num">
-                        </div>
-                        <div class="add">
-                            <button class="addcar">加入购物车</button>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-                `
-            }).join("");
-            $(".p-conter-c").html(html);
-        }
-    });
-
-
 })
